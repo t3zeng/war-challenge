28fcc7a854a35d08c00f5f784c821cfdaf04514e
Tian Zeng <t3zeng@gmail.com>
2022-10-12 23:42:54 -0400

    war card game

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Tianyu Zeng on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
